c43fb04621eab8ab9a06956552578fe413765c23
blackrose54 <bhakatabhinav54@gmail.com>
2025-08-02 13:35:53 +0530

    updated seed.py; took into account both 6m and all csv

--- backend/seed.py
+++ backend/seed.py
@@ -25,6 +25,10 @@ for dirpath,dirname,filenames in os.walk('../leetcode-company-wise-problems/'):
     if dirpath.startswith('../leetcode-company-wise-problems/.git') or dirpath.startswith('./.idea') or dirpath == '../leetcode-company-wise-problems/':
         continue
 
+    curr_question = []
+    
+    all_questions = []
+
     with open(os.path.join(dirpath,'4. More Than Six Months.csv')) as f:
         reader = csv.reader(f)
         for row in reader:
@@ -42,8 +46,34 @@ for dirpath,dirname,filenames in os.walk('../leetcode-company-wise-problems/'):
                     "Link": row[4],
                     "Topics":[x.replace('(Priority Queue)','').strip().capitalize() for x in row[5].split(',')]
                 }
-                questoins.append(d)
+                curr_question.append(d)
+                
+    with open(os.path.join(dirpath,'5. All.csv')) as f:
+        reader = csv.reader(f)
+        for row in reader:
 
+            if row[2] == 'Frequency':
+                continue
+
+            if len(row) == 6:
+                d = {
+                    "Company":os.path.basename(dirpath).capitalize(),
+                    "Difficulty": row[0].upper(),
+                    "Title": row[1].capitalize(),
+                    "Frequency": row[2],
+                    "Acceptance Rate": row[3],
+                    "Link": row[4],
+                    "Topics":[x.replace('(Priority Queue)','').strip().capitalize() for x in row[5].split(',')]
+                }
+
+                all_questions.append(d)
+    
+    if len(curr_question) > len(all_questions):
+        questoins.extend(curr_question)         
+    else:
+        questoins.extend(all_questions)
+
+    
 
 client.insert_many(questoins)
 print('Added to DB')
